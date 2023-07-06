d5df87267fa6797f5bcf0a20b76750fa4a2b3c3f
Valera <vlaus0@gmail.com>
2023-07-06 23:05:33 +0300

    Добавьте файлы проекта.

---
+++ Task01/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Task01.Models;
+
+namespace Task01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(FormModel model)
+        {
+            _logger.LogInformation("First: {First}", model.First);
+            _logger.LogInformation("Second: {Second}", model.Second);
+            _logger.LogInformation("Count: {Count}", model.Count);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Task01/Models/FormModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Task01.Models
+{
+    public class FormModel
+    {
+        public string First { get; set; }
+        public string Second { get; set; }
+        public int Count { get; set; }
+    }
+
+}
